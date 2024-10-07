2a710c7c8f3e766e1d49d0cf8261c9149720b760
Marcos Vinicius <marcos.lightbase@sebraemg.com.br>
2024-10-07 16:18:46 -0300

    implementa método verifica pilha vazia

--- src/com/marcos/estruturadados/base/EstruturaEstatica.java
+++ src/com/marcos/estruturadados/base/EstruturaEstatica.java
@@ -15,6 +15,11 @@ public class EstruturaEstatica<T> {
         this(10);
     }
 
+    public boolean estaVazia() {
+        return this.tamanho == 0;
+    }
+
+
     protected boolean adiciona(T elemento) {
         this.aumentaCapacidade();
         if(this.tamanho < this.elementos.length) {
