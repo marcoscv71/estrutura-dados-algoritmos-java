15e415724d6047aad8764d174a09e88192a83610
Marcos Vinicius <marcos.lightbase@sebraemg.com.br>
2024-10-07 16:03:34 -0300

    Refatorando listas e pilha na classe estrutura estatica

---
+++ src/com/marcos/estruturadados/pilha/Pilha.java
@@ -0,0 +1,14 @@
+package com.marcos.estruturadados.pilha;
+
+import com.marcos.estruturadados.base.EstruturaEstatica;
+
+public class Pilha<T> extends EstruturaEstatica<T> {
+
+    public Pilha() {
+        super();
+    }
+
+    public Pilha(int capacidade) {
+        super(capacidade);
+    }
+}
---
+++ src/com/marcos/estruturadados/vetor/Lista2.java
@@ -0,0 +1,19 @@
+package com.marcos.estruturadados.vetor;
+
+import com.marcos.estruturadados.base.EstruturaEstatica;
+
+public class Lista2<T> extends EstruturaEstatica<T> {
+
+    public Lista2(int capacidade) {
+        super(capacidade);
+    }
+
+    public boolean adiciona(T elemento) {
+        return super.adiciona(elemento);
+    }
+
+    public boolean adiciona(int posicao, T elemento) {
+        return super.adiciona(posicao, elemento);
+    }
+
+}
