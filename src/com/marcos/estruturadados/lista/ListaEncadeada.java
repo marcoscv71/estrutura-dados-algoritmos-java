d1421676008bbf79968ed4d48588c25d81088590
Marcos Vinícius Silva <marcoscv71@gmail.com>
2024-10-15 08:08:46 -0300

    Adicionando o metodo para inserir em qualquer posicao da lista

--- src/com/marcos/estruturadados/lista/ListaEncadeada.java
+++ src/com/marcos/estruturadados/lista/ListaEncadeada.java
@@ -2,12 +2,23 @@ package com.marcos.estruturadados.lista;
 
 public class ListaEncadeada<T> {
 
+
     private No<T> inicio;
+    private No<T> ultimo;
+
+    private int tamanho = 0;
 
     public void adiciona(T elemento) {
         No<T> celula = new No<T>(elemento);
         this.inicio = celula;
+        this.ultimo = celula;
+        this.tamanho++;
     }
+
+    public int getTamanho() {
+        return this.tamanho;
+    }
+
 
     @Override
     public String toString() {
--- src/com/marcos/estruturadados/lista/teste/ListaEncadeadaTeste.java
+++ src/com/marcos/estruturadados/lista/teste/ListaEncadeadaTeste.java
@@ -5,9 +5,12 @@ import com.marcos.estruturadados.lista.ListaEncadeada;
 public class ListaEncadeadaTeste {
 
     public static void main(String[] args) {
+
         ListaEncadeada<Integer> lista = new ListaEncadeada<>();
+
         lista.adiciona(1);
 
+        System.out.println("Tamanho = " + lista.getTamanho());
         System.out.println(lista);
     }
 }
